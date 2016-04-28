eaa2dc4c112d36e52aed8c48fe3d7885d1d9c063
Renne Rocha <renne@rennerocha.com>
2016-04-27 23:52:48 -0300

    notificação PayPal

--- lhc.py
+++ lhc.py
@@ -1,4 +1,6 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import datetime
+
 from flask import Flask, g, jsonify, request
 
 import models
@@ -10,6 +12,7 @@ app = Flask(__name__)
 def before_request():
     g.db = models.db
     g.db.connect()
+
 
 @app.after_request
 def after_request(response):
@@ -42,7 +45,49 @@ def payment():
         new_entry.save()
         return jsonify(**new_entry.to_dict()), 201
     else:
-        return jsonify({'status':'Bad Request'}), 400
+        return jsonify({'status': 'Bad Request'}), 400
+
+
+@app.route('/notification/paypal/', methods=['POST', 'GET', ])
+def paypal():
+    txn_type = request.values.get('txn_type', None)
+    item_name = request.values.get('item_name', '')
+    first_name = request.values.get('first_name', '')
+    last_name = request.values.get('last_name', '')
+
+    if txn_type in ['subscr_payment', 'web_accept']:  # Payment received
+        payment_date = request.values.get('payment_date', '')
+        payment_date = datetime.datetime.strptime(payment_date, '%H:%M:%S %b %d, %Y PST')
+        mc_gross = request.values.get('mc_gross', '')
+        if txn_type == 'subscr_payment':
+            payment_tags = ['mensalidade', ]
+            tax_payment_tags = ['mensalidade', 'taxa']
+        elif txn_type == 'web_accept':
+            payment_tags = ['doacao', ]
+            tax_payment_tags = ['doacao', 'taxa', ]
+
+        payment_description = u'{0} - {1} {2}'.format(item_name, first_name, last_name)
+        tax_payment_description = u'{0} - Taxa {1} {2}'.format(item_name, first_name, last_name)
+
+        new_entry = models.Entry(
+            entry_date=payment_date.strftime('%Y-%m-%d'),
+            value='%.2f' % float(mc_gross),
+            account='_paypal',
+            tags=','.join(payment_tags),
+            description=payment_description,
+        )
+        new_entry.save()
+
+        new_entry = models.Entry(
+            entry_date=payment_date.strftime('%Y-%m-%d'),
+            value='%.2f' % float(mc_gross),
+            account='_paypal',
+            tags=','.join(tax_payment_tags),
+            description=tax_payment_description,
+        )
+        new_entry.save()
+
+    return "OK"
 
 
 @app.route('/report', methods=['GET', ])
@@ -61,50 +106,15 @@ if __name__ == "__main__":
     app.run()
 
 
-
-
-
-
-
-# import datetime
-# import logging
-
-# from flask import Flask, request
-
-
-# logger = logging.getLogger("payments")
-# logger.setLevel(logging.INFO)
-
-# # create a file handler
-# handler = logging.FileHandler('payments.log')
-# handler.setLevel(logging.INFO)
-
-# # create a logging format
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
-
-# # add the handlers to the logger
-# logger.addHandler(handler)
-
-# app = Flask("app")
-# app.debug = True
-
-# PAYMENT_TEMPLATE = '{payment_date}\t{payment_value}\t\t{payment_tags}|{payment_description}'
-
-
 # @app.route("/notification/paypal/", methods=["POST", "GET", ])
 # def paypal():
 #     received_data = str(request.values)
-#     logger.info(received_data)
 
 #     txn_type = request.values.get('txn_type', None)
 #     item_name = request.values.get('item_name', '')
 #     first_name = request.values.get('first_name', '')
 #     last_name = request.values.get('last_name', '')
 
-#     logger.info(txn_type)
-
-#     new_lines = []
 #     if txn_type in ['subscr_payment', 'web_accept']:  # Payment received
 #         payment_date = request.values.get('payment_date', '')
 #         payment_date = datetime.datetime.strptime(payment_date, '%H:%M:%S %b %d, %Y PST')
@@ -158,39 +168,3 @@ if __name__ == "__main__":
 #     update_html_report()
 
 #     return "OK"
-
-
-# @app.route("/notification/payment/", methods=["POST", "GET", ])
-# def payment():
-#     received_data = str(request.values)
-#     logger.info(received_data)
-
-#     new_lines = []
-#     new_lines.append(PAYMENT_TEMPLATE.format(**{
-#         'payment_date': request.values.get('payment_date').encode('utf-8'),
-#         'payment_value': '%.2f' % float(request.values.get('payment_value').encode('utf-8')),
-#         'payment_tags': request.values.get('payment_tags').encode('utf-8'),
-#         'payment_description': request.values.get('payment_description').encode('utf-8')
-#     }))
-
-#     with open('lhc2016real.txt', 'a') as money_file:
-#         for line in new_lines:
-#             money_file.write(line)
-#             money_file.write('\n')
-
-#     update_html_report()
-
-#     return "OK"
-
-
-# def update_html_report():
-#     with open('lhc2016real.txt', 'r') as money_file:
-#         money_data = money_file.read()
-
-#     with open('money_template.html', 'r') as money_template:
-#         template = money_template.read()
-#         template = template.replace('[CONTEUDO]', money_data)
-
-#     with open('/var/www/apps/blog/lhc.html', 'w') as final_report:
-#         final_report.write(template)
-
