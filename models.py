774f19e3ef26c87458053093158384100a0cba31
Renne Rocha <renne@rennerocha.com>
2016-04-27 21:07:08 -0300

    BaseModel

--- models.py
+++ models.py
@@ -1,19 +1,35 @@
+import datetime
+import json
 import peewee
-
+from playhouse.shortcuts import model_to_dict
 
 db = peewee.SqliteDatabase('lhc.db')
 
 
-class Entry(peewee.Model):
+class BaseModel(peewee.Model):
+
+    class Meta:
+        database = db
+
+    def to_dict(self, json_safe=True, datefield_format='%Y-%m-%d'):
+        dict_model = model_to_dict(self)
+
+        if json_safe:  # make returned dict serializable by json library
+            for key, value in dict_model.items():
+                if isinstance(value, datetime.date):
+                    new_value = value.strftime(datefield_format)
+                    dict_model[key] = new_value
+
+        return dict_model
+
+
+class Entry(BaseModel):
 
     entry_date = peewee.DateField()
     value = peewee.DecimalField()
     account = peewee.CharField()
     tags = peewee.CharField
     description = peewee.TextField()
-
-    class Meta:
-        database = db
 
 
 db.create_tables([Entry, ], safe=True)
