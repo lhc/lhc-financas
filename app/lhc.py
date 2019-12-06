cea3d0265ee2dab4105cbefd5d4aa32ad862dd5e
Renne Rocha <renne@rennerocha.com>
2019-12-06 12:51:13 -0300

    Ignorar aporte inicial nos recebimentos

--- app/lhc.py
+++ app/lhc.py
@@ -8,50 +8,52 @@ from flask import Flask, jsonify, request, render_template
 
 import models
 
-PAYPAL_PRODUCTION = 'https://ipnpb.paypal.com/cgi-bin/webscr'
+PAYPAL_PRODUCTION = "https://ipnpb.paypal.com/cgi-bin/webscr"
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 def valid_entry(entry):
     # All fields are required
-    return all([
-        entry.get('entry_date'),
-        entry.get('value'),
-        entry.get('account'),
-        entry.get('tags'),
-        entry.get('description'),
-    ])
+    return all(
+        [
+            entry.get("entry_date"),
+            entry.get("value"),
+            entry.get("account"),
+            entry.get("tags"),
+            entry.get("description"),
+        ]
+    )
 
 
-@app.route('/new_entry', methods=['POST', ])
+@app.route("/new_entry", methods=["POST"])
 def new_entry():
     content = request.get_json()
     if valid_entry(content):
         new_entry = models.Entry(
-            entry_date=content.get('entry_date'),
-            value=content.get('value'),
-            account=content.get('account'),
-            tags=content.get('tags'),
-            description=content.get('description'),
+            entry_date=content.get("entry_date"),
+            value=content.get("value"),
+            account=content.get("account"),
+            tags=content.get("tags"),
+            description=content.get("description"),
         )
         new_entry.save()
         return jsonify(**new_entry.to_dict()), 201
     else:
-        return jsonify({'status': 'Bad Request'}), 400
+        return jsonify({"status": "Bad Request"}), 400
 
 
-@app.route('/report')
-@app.route('/report/<year>')
+@app.route("/report")
+@app.route("/report/<year>")
 def report(year=None):
     if year is None:
         year = datetime.date.today().year
-    report_file = 'lhc_{year}.html'.format(year=year)
+    report_file = "lhc_{year}.html".format(year=year)
     return render_template(report_file)
 
 
-@app.route('/status')
+@app.route("/status")
 def status():
     month = datetime.date.today().month
     year = datetime.date.today().year
@@ -61,22 +63,24 @@ def status():
     past_year = year - 1 if past_month == 12 else year
     next_year = year + 1 if next_month == 1 else year
 
-    regular_expenses_tags = ['aluguel', 'cpfl', 'net', 'sanasa', 'vivo']
+    regular_expenses_tags = ["aluguel", "cpfl", "net", "sanasa", "vivo"]
 
     regular_expenses_actual_month = models.Entry.select().where(
-        (models.Entry.entry_date >= datetime.date(year, month, 1)) &
-        (models.Entry.entry_date < datetime.date(next_year, next_month, 1)) &
-        (models.Entry.tags.in_(regular_expenses_tags))
+        (models.Entry.entry_date >= datetime.date(year, month, 1))
+        & (models.Entry.entry_date < datetime.date(next_year, next_month, 1))
+        & (models.Entry.tags.in_(regular_expenses_tags))
     )
     regular_expenses_actual = abs(
-        sum([entry.value for entry in regular_expenses_actual_month]))
+        sum([entry.value for entry in regular_expenses_actual_month])
+    )
     regular_expenses_past_month = models.Entry.select().where(
-        (models.Entry.entry_date < datetime.date(year, month, 1)) &
-        (models.Entry.entry_date >= datetime.date(past_year, past_month, 1)) &
-        (models.Entry.tags.in_(regular_expenses_tags))
+        (models.Entry.entry_date < datetime.date(year, month, 1))
+        & (models.Entry.entry_date >= datetime.date(past_year, past_month, 1))
+        & (models.Entry.tags.in_(regular_expenses_tags))
     )
     regular_expenses_past = abs(
-        sum([entry.value for entry in regular_expenses_past_month]))
+        sum([entry.value for entry in regular_expenses_past_month])
+    )
 
     tags_already_spent = []
     regular_expenses_estimate = 0
@@ -88,89 +92,88 @@ def status():
             regular_expenses_estimate += abs(entry.value)
 
     actual_incomes = models.Entry.select().where(
-        (models.Entry.entry_date < datetime.date(next_year, next_month, 1)) &
-        (models.Entry.entry_date >= datetime.date(year, month, 1)) &
-        (models.Entry.value > 0) &
-        (~models.Entry.tags.in_(['transferencia', 'inicial', ]))
+        (models.Entry.entry_date < datetime.date(next_year, next_month, 1))
+        & (models.Entry.entry_date >= datetime.date(year, month, 1))
+        & (models.Entry.value > 0)
+        & (~models.Entry.tags.in_(["transferencia", "inicial", "aporteinicial"]))
     )
-    total_incomes = abs(
-        sum([entry.value for entry in actual_incomes]))
+    total_incomes = abs(sum([entry.value for entry in actual_incomes]))
 
     actual_expenses = models.Entry.select().where(
-        (models.Entry.entry_date < datetime.date(next_year, next_month, 1)) &
-        (models.Entry.entry_date >= datetime.date(year, month, 1)) &
-        (models.Entry.value < 0) &
-        (~models.Entry.tags.in_(regular_expenses_tags)) &
-        (~models.Entry.tags.in_(['transferencia', 'inicial', ]))
+        (models.Entry.entry_date < datetime.date(next_year, next_month, 1))
+        & (models.Entry.entry_date >= datetime.date(year, month, 1))
+        & (models.Entry.value < 0)
+        & (~models.Entry.tags.in_(regular_expenses_tags))
+        & (~models.Entry.tags.in_(["transferencia", "inicial", "aporteinicial"]))
     )
-    total_expenses = abs(
-        sum([entry.value for entry in actual_expenses]))
+    total_expenses = abs(sum([entry.value for entry in actual_expenses]))
 
-    return jsonify({
-        "actual_incomes": str(total_incomes),
-        "actual_expenses": str(total_expenses),
-        "regular_expenses_estimate": str(regular_expenses_estimate),
-        "regular_expenses_actual": str(regular_expenses_actual),
-        "regular_expenses_past": str(regular_expenses_past),
-    }), 200
+    return (
+        jsonify(
+            {
+                "actual_incomes": str(total_incomes),
+                "actual_expenses": str(total_expenses),
+                "regular_expenses_estimate": str(regular_expenses_estimate),
+                "regular_expenses_actual": str(regular_expenses_actual),
+                "regular_expenses_past": str(regular_expenses_past),
+            }
+        ),
+        200,
+    )
 
 
-@app.route('/paypal/notification', methods=['POST', ])
+@app.route("/paypal/notification", methods=["POST"])
 def paypal_notification():
-    ''' Listener for PayPal notification '''
+    """ Listener for PayPal notification """
     notification = request.form.to_dict()
 
-    notification['cmd'] = '_notify-validate'
+    notification["cmd"] = "_notify-validate"
     headers = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'host': 'www.paypal.com'
+        "content-type": "application/x-www-form-urlencoded",
+        "host": "www.paypal.com",
     }
     validation_response = requests.post(
-        PAYPAL_PRODUCTION,
-        data=notification,
-        headers=headers,
-        verify=True)
+        PAYPAL_PRODUCTION, data=notification, headers=headers, verify=True
+    )
     validation_response.raise_for_status()
 
-    valid_txn_types = ('subscr_payment', 'web_accept', )
-    txn_type = notification.get('txn_type')
-    if validation_response.text == 'VERIFIED' and txn_type in valid_txn_types:
-        full_name = ' '.join([
-            notification.get('first_name', ''),
-            notification.get('last_name', ''),
-        ])
-        value = notification.get('mc_gross', '0')
-        tax_value = '-{}'.format(notification.get('mc_fee', '0'))
+    valid_txn_types = ("subscr_payment", "web_accept")
+    txn_type = notification.get("txn_type")
+    if validation_response.text == "VERIFIED" and txn_type in valid_txn_types:
+        full_name = " ".join(
+            [notification.get("first_name", ""), notification.get("last_name", "")]
+        )
+        value = notification.get("mc_gross", "0")
+        tax_value = "-{}".format(notification.get("mc_fee", "0"))
 
-        payment_date = notification.get('payment_date', '')
-        match = re.findall('[A-Za-z]{3} [0-9]{2}, [0-9]{4}', payment_date)
+        payment_date = notification.get("payment_date", "")
+        match = re.findall("[A-Za-z]{3} [0-9]{2}, [0-9]{4}", payment_date)
         if match:
             raw_payment_date = match.pop()
-            entry_date = datetime.datetime.strptime(raw_payment_date, '%b %d, %Y')
-            entry_date = entry_date.strftime('%Y-%m-%d')
+            entry_date = datetime.datetime.strptime(raw_payment_date, "%b %d, %Y")
+            entry_date = entry_date.strftime("%Y-%m-%d")
 
-        account = '_paypal'
-        description = '{} - {}'.format(
-            notification.get('item_name'),
-            full_name)
-        tax_description = 'Taxa - {}'.format(description)
+        account = "_paypal"
+        description = "{} - {}".format(notification.get("item_name"), full_name)
+        tax_description = "Taxa - {}".format(description)
 
         tags_map = {
-            'camiseta-30': 'camisetas',
-            'lhc-85': 'mensalidade',
-            'lhc-110': 'mensalidade',
-            'lhc-60': 'mensalidade',
-            'lhc-30': 'contribuicao',
-            'doacao-lhc': 'doacao',
+            "camiseta-30": "camisetas",
+            "lhc-85": "mensalidade",
+            "lhc-110": "mensalidade",
+            "lhc-60": "mensalidade",
+            "lhc-30": "contribuicao",
+            "doacao-lhc": "doacao",
         }
-        tags = tags_map.get(notification.get('item_number', ''), '')
+
+        tags = tags_map.get(notification.get("item_number", ""), "")
 
         entry = models.Entry(
             entry_date=entry_date,
             value=value,
             account=account,
             tags=tags,
-            description=description
+            description=description,
         )
         entry.save()
 
@@ -178,12 +181,12 @@ def paypal_notification():
             entry_date=entry_date,
             value=tax_value,
             account=account,
-            tags='taxa,{}'.format(tags),
-            description=tax_description
+            tags="taxa,{}".format(tags),
+            description=tax_description,
         )
         tax_entry.save()
 
-    return ''
+    return ""
 
 
 if __name__ == "__main__":
